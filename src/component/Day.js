f11c89e8046cf493e60f453ff3e86c3e2855f075
sh970901 <aaddss639@naver.com>
2022-01-03 01:02:22 +0900

    2_note

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,19 +1,25 @@
 //데이에 맞는 word들을 보여줌
-
-
-import React from 'react'
-import dummy from "../db/data.json"
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Word from './Word'
-
+import { useEffect} from 'react'
+ 
 const Day = () => {
     const a=  useParams();
     // console.log(a)
     const day = a.day;
-    const wordList = dummy.words.filter(word =>(
-        word.day === Number(day)
-        //숫자로 맞춰줘야함 
-    ))
+    
+    const [words, setWords] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`) //api 비동기 통신을 위한 fetch
+        .then(res=> {
+            return res.json();  //res는 html이기때문에 json으로 변환해주어야함 
+        })
+        .then(data =>{
+            setWords(data);
+            console.log(data)
+        })
+    }, [day]); //특정값 ${day}을 사용하였기 때문에 의존성문제때문에 day를 넣어줘야 ${day}이 값이 최신값임을 알게됨
  
     
 //useParams 값은 App.js에 /:id라고하면 id값으로 받아오고 /:day라 하면 day값을 받아옴 console.log로 확인  
@@ -22,7 +28,7 @@ const Day = () => {
             <h2>Day{day}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word =>(                      
+                    {words.map(word =>(                      
                     <Word word= {word} key = {word.id}></Word>
                     ))}
                 </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,19 +1,42 @@
 //Day들을 표시 추가.삭제가능
 
-import React from 'react'
-import dummy from "../db/data.json"
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+//import useFetch from '../hooks/useFetch';
 
 const DayList = () => {
-    console.log(dummy);
+    const [days, setDays] = useState([]);
+
+
+   // const days = useFetch("http://localhost:3001/days");
+
+
+    //useEffect는 렌더링이 끝나고 실행됨 
+    useEffect(() => {
+        fetch('http://localhost:3001/days') //api 비동기 통신을 위한 fetch
+        .then(res=> {
+            return res.json();  //res는 html이기때문에 json으로 변환해주어야함 
+        })
+        .then(data =>{
+            setDays(data);
+            console.log(data)
+        })
+    }, []); //빈배열이므로 한번만 실행됨 만약 카운트가 들어오면 카운트 증가할때만 실행됨 
+
+
     return (
-        <ul className='list_day'>
-            {dummy.days.map(day => (             //day 개수만큼 뽑아줘야하는데 반복문 대신 map을 이용, map은 배열을 받아서 또 다른 배열을 반환함 , 키는 반복되는 요소의 고유한 값을 넣어주어야함
-                <li key={day.id}>
-                    <Link to= {`/day/${day.day}`}>Day {day.day}</Link>
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul className='list_day'>
+                {days.map(day => (             //day 개수만큼 뽑아줘야하는데 반복문 대신 map을 이용, map은 배열을 받아서 또 다른 배열을 반환함 , 키는 반복되는 요소의 고유한 값을 넣어주어야함
+                    <li key={day.id}>
+                        <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+                    </li>
+                ))}
+            </ul>
+            
+        </>
+
     )
 }
 
